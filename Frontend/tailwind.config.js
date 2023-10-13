9cfead2237574705d52a171c3c09ba0b4ebdff6a
Timothy Avell <timothy.avell.olatunde@gmail.com>
2023-10-13 14:40:45 -0400

    Finished tailwindcss comfiguration and setup

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}', // Your React source files
+  ],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
